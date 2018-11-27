c19012599809dd5dd02e2f386587be291b8abd64
BebopBobby <bbbgalvin4@gmail.com>
2018-11-27 09:16:46 -0500

    final commit

---
+++ src/main/java/dao/IArticleDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import entity.Article;
+
+import java.util.List;
+
+public interface IArticleDAO {
+    List<Article> getAllArticles();
+    Article getArticleById(int articleId);
+    void createArticle(Article article);
+    void updateArticle(Article article);
+    void deleteArticle(int articleId);
+    boolean articleExists(String title, String category);
+}
